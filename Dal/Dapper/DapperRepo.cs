e33043b158b3655724375bee6380283ce6bb5104
Todd Behunin <tbehunin@gmail.com>
2016-01-29 10:15:34 -0800

    Added a dapper repo

---
+++ Dal/Dapper/DapperRepo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Dapper
+{
+    public class DapperRepo : IRepository
+    {
+        public int AddBrand(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBrand(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Brand> GetBrands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Brand GetBrandById(int brandId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Brand GetProductById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dal/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dal.Dapper;
+
+namespace Dal
+{
+    public interface IRepository
+    {
+        int AddBrand(Brand brand);
+        void UpdateBrand(Brand brand);
+        IList<Brand> GetBrands();
+        Brand GetBrandById(int brandId);
+
+        int AddProduct(Product product);
+        void UpdateProduct(Product product);
+        IList<Product> GetProducts();
+        Brand GetProductById(int productId);
+    }
+}
