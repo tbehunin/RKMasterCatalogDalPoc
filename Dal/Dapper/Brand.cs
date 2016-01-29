b703b74acd894c95ed631b32891381ff7a80dc45
Todd Behunin <tbehunin@gmail.com>
2016-01-29 10:05:02 -0800

    Added poco classes and dapper lib

---
+++ Dal/Dapper/Brand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Dapper
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string BrandCode { get; set; }
+        public string BrandDescription { get; set; }
+        public string BrandDisplayName { get; set; }
+        public string InCommBrandIdentifier { get; set; }
+        public string BrandImageUrl { get; set; }
+        public string IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ Dal/Dapper/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Dapper
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductDisplayName { get; set; }
+        public Brand Brand { get; set; }
+        public string InCommProductUPC { get; set; }
+        public string ProductSKU { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public string TempInfoBlob { get; set; }
+    }
+}
