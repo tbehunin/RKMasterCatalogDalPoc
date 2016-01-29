bb075de4194b649e5c855c173f7fee6bbfd7c126
Todd Behunin <tbehunin@gmail.com>
2016-01-29 15:01:52 -0800

    Added brands injector & tests.

---
+++ Test/Dapper/BrandInjector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dal.Dapper;
+
+namespace Test.Dapper
+{
+    public class BrandInjector
+    {
+        public static DapperRepo Repo;
+
+        public static int Inject()
+        {
+            Repo = new DapperRepo(ConfigurationManager.ConnectionStrings["GDBConnStr"].ConnectionString);
+            var guid = Guid.NewGuid().ToString();
+            var brand = new Brand
+            {
+                BrandName = "Name " + guid,
+                BrandCode = "Code " + guid,
+                BrandDescription = "Descr " + guid,
+                BrandDisplayName = "Display " + guid,
+                InCommBrandIdentifier = "Brand id " + guid,
+                BrandImageUrl = "ImageUrl " + guid,
+                IsActive = true,
+                CreatedOn = DateTime.Now,
+                CreatedBy = "CreatedByFoo",
+                ModifiedOn = DateTime.Now,
+                ModifiedBy = "ModifiedByFoo"
+            };
+            brand.BrandId = Repo.AddBrand(brand);
+            return brand.BrandId;
+        }
+    }
+}
---
+++ Test/Dapper/GetBrandsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Dapper
+{
+    /// <summary>
+    /// Summary description for GetBrandsTests
+    /// </summary>
+    [TestClass]
+    public class GetBrandsTests
+    {
+        public GetBrandsTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
